10d668ee7aba56afdf7d4ebf134f82a0a649d820
axios <axioscros@aliyun.com>
2018-12-11 18:43:47 +0800

    debug fixed

--- library/core/Builder.php
+++ library/core/Builder.php
@@ -410,7 +410,6 @@ abstract class Builder
         }
         $bindType = isset($binds[$field]) ? $binds[$field] : PDO::PARAM_STR;
         if (is_scalar($value) && array_key_exists($field, $binds) && !in_array($exp, ['EXP', 'NOT NULL', 'NULL', 'FIND_IN_SET', 'IN', 'NOT IN', 'BETWEEN', 'NOT BETWEEN']) && strpos($exp, 'TIME') === false) {
-            dd('in');
             if (strpos($value, ':') !== 0 || !$this->query->isBind(substr($value, 1))) {
                 if ($this->query->isBind($bindName)) {
                     $bindName .= '_' . str_replace('.', '_', uniqid('', true));
