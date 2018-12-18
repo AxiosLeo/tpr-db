ef589899564a105646c5086dd570b7b911c6126e
axios <axioscros@aliyun.com>
2018-12-18 16:23:55 +0800

    数据库管理接口

---
+++ library/manager/driver/DriverInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author  : axios
+ * @email   : axiosleo@foxmail.com
+ * @blog    : http://hanxv.cn
+ * @datetime: 2018-12-18 14:54
+ */
+
+namespace tpr\db\manager\driver;
+
+interface DriverInterface
+{
+    public function createDatabase();
+
+    public function createTable();
+
+    public function createField();
+}
