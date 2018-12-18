7d6fb690cf73f16a2cdeaebc6a975c4960ab5686
axios <axioscros@aliyun.com>
2018-12-18 16:24:18 +0800

    mysql数据库管理操作

---
+++ library/manager/driver/Mysql.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author  : axios
+ * @email   : axiosleo@foxmail.com
+ * @blog    : http://hanxv.cn
+ * @datetime: 2018-12-18 14:55
+ */
+
+namespace tpr\db\manager\driver;
+
+class Mysql extends Driver implements DriverInterface
+{
+    public function createDatabase()
+    {
+        // TODO: Implement createDatabase() method.
+    }
+
+    public function createTable()
+    {
+        // TODO: Implement createTable() method.
+    }
+
+    public function createField()
+    {
+        // TODO: Implement createField() method.
+    }
+}
