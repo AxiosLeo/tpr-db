41799bdc19128d8af5921dfbe86fdc88780b3cd4
axios <axioscros@aliyun.com>
2018-12-19 15:11:34 +0800

    字符集

--- library/manager/mysql/Charset.php
+++ library/manager/mysql/Charset.php
@@ -10,5 +10,17 @@ namespace tpr\db\manager\mysql;
 
 class Charset
 {
-    const Utf8 = 'utf8';
+    const Utf8     = 'utf8';
+    const Armscii8 = 'armscii8';
+    const Ascii    = 'Ascii';
+    const Big5     = 'big5';
+    const Binary   = 'binary';
+    const Cp1250   = 'cp1250';
+    const Cp1251   = 'cp1251';
+    const Cp1256   = 'cp1257';
+    const Gb2312   = 'gb2312';
+    const Gbk      = 'gbk';
+    const Utf8mb4  = 'utf8mb4';
+    const Utf16    = 'utf16';
+    const Utf32    = 'utf32';
 }
