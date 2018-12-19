8dafd0471bde0e86f608599adaa84987fdb34fbe
axios <axioscros@aliyun.com>
2018-12-19 16:19:23 +0800

    增加基类方法

--- library/manager/driver/Mysql.php
+++ library/manager/driver/Mysql.php
@@ -8,7 +8,10 @@
 
 namespace tpr\db\manager\driver;
 
+use tpr\db\manager\mysql\Charset;
+use tpr\db\manager\mysql\Collate;
 use tpr\db\manager\mysql\Database;
+use tpr\db\manager\mysql\Sql;
 
 class Mysql extends Driver
 {
@@ -19,11 +22,9 @@ class Mysql extends Driver
      */
     private static $DatabaseInstance;
 
-    public function __construct($query)
-    {
-        parent::__construct($query);
-        $this->db_name = $this->options->get('database');
-    }
+    protected $charset = Charset::Utf8;
+
+    protected $collate = Collate::General;
 
     /**
      * @param null $db_name
@@ -36,12 +37,52 @@ class Mysql extends Driver
             $db_name = $this->db_name;
         }
         if (is_null(self::$DatabaseInstance)) {
-            self::$DatabaseInstance = new Database($this->query);
+            self::$DatabaseInstance = new Database();
             self::$DatabaseInstance->setDatabaseName($db_name);
         } elseif (!is_null($db_name) && $db_name != $this->db_name) {
             $this->db_name = $db_name;
             self::$DatabaseInstance->setDatabaseName($this->db_name);
         }
         return self::$DatabaseInstance;
+    }
+
+    public function setCharset($charset)
+    {
+        $this->charset = $charset;
+    }
+
+    public function setCollate($collate)
+    {
+        $this->collate = $collate;
+    }
+
+    protected function getDataType($charset = '', $collate = '')
+    {
+        if (empty($charset) && empty($collate)) {
+            return '';
+        }
+        $data = [
+            'charset' => $charset,
+            'collate' => $collate
+        ];
+
+        return Sql::getSql(Sql::DATATYPE, $data);
+    }
+
+    protected function formatDbName($db_name)
+    {
+        return '`' . $db_name . '`';
+    }
+
+    protected function formatTableName($table_name)
+    {
+        $prefix  = self::$query->getConfig('prefix', '');
+        $db_name =  self::$query->getConfig('database');
+        return $this->formatDbName($db_name) . '.`' . $prefix . $table_name . '`';
+    }
+
+    protected function formatColumn($column_name)
+    {
+        return '`' . $column_name . '`';
     }
 }
