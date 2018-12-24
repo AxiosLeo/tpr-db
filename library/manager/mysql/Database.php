c1ffe62c80abe11c19b15bb84a49b9dabb560a8d
axios <axioscros@aliyun.com>
2018-12-24 10:20:39 +0800

    转存sql

--- library/manager/mysql/Database.php
+++ library/manager/mysql/Database.php
@@ -66,4 +66,92 @@ class Database extends Mysql
         $this->operation = Operation::DB_DELETE;
         return $this;
     }
+
+    public function saveAllData($path = '', $name = '')
+    {
+        if (!file_exists($path)) {
+            return false;
+        }
+        if (empty($name)) {
+            $name = $this->dbName();
+        }
+        $filename = $path . DIRECTORY_SEPARATOR . $name . '.sql';
+        if (file_exists($filename)) {
+            @unlink($filename);
+        }
+        $tables = $this->getTableList();
+
+        // create database sql
+        $this->saveFile($filename, $this->create()->buildSql() . ';');
+
+        $n = 0;
+        foreach ($tables as $table) {
+            $table_name = '`' . $table . '`';
+
+            // create table sql
+            $this->saveFile($filename, $this->getSql($this->query->query("SHOW CREATE TABLE " . $table_name)), 3);
+            // insert data sql
+            $tmp = $this->query->table($table)->count();
+            while ($tmp > 0) {
+                $limit = 10;
+                $data  = $this->query->table($table)->limit($limit)->select();
+                foreach ($data as $d) {
+                    $this->saveFile($filename, $this->buildDataSql($table_name, $d));
+                }
+                $tmp = $tmp - $limit;
+            }
+            $n++;
+        }
+        return true;
+    }
+
+    private function saveFile($filename, $sql, $blank = 0)
+    {
+        $fp = fopen($filename, 'a+');
+        if (flock($fp, LOCK_EX)) {
+            while ($blank > 0) {
+                fwrite($fp, "\r\n");
+                $blank = $blank - 1;
+            }
+            fwrite($fp, $sql . "\r\n");
+            flock($fp, LOCK_UN);
+        }
+        fclose($fp);
+    }
+
+    private function getSql($result)
+    {
+        foreach ($result as $r) {
+            $n = 0;
+            foreach ($r as $t) {
+                $n++;
+                if ($n == 2) {
+                    return $t;
+                }
+            }
+        }
+        return "";
+    }
+
+    private function buildDataSql($table, $data)
+    {
+        $values = "";
+        $n      = 0;
+        foreach ($data as $d) {
+            if ($n > 0) {
+                $values .= ",";
+            }
+            if (!is_null($d)) {
+                $values .= "'" . $d . "'";
+            } else {
+                $values .= 'null';
+            }
+            $n++;
+        }
+
+        return Sql::getSql('insert.data', [
+            'table_name' => $table,
+            'values'     => $values
+        ]);
+    }
 }
